2c841119d3f7eb1002b020c6400aa191541eced5
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2020-11-25 21:08:12 +0700

    redploy changes

--- dist/_nuxt/c37318e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{709:function(e,t,n){"use strict";n.r(t);n(30),n(48);var r={mounted:function(){this.$router.replace("/login")}},l=n(44),component=Object(l.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("p",[this._v("Please wait redirecting...")])}),[],!1,null,null,null);t.default=component.exports}}]);
